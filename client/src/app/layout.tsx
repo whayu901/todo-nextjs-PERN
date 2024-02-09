c0b8bbfcc19f27109539c24e7cabaa70d9842abb
Cahyono Wahyu Setiawan <cahyonowahyus123@gmail.com>
2024-02-09 22:38:20 +0700

    Feature: setup global css tailwind

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-night">{children}</body>
     </html>
   );
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,12 +6,24 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
+      colors: {
+        night: "#0a0a0a",
+        midnight: "#5a5a5a",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
