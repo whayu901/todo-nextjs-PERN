80dd78f3a523a47c0a7afaf1987c73543ea514d3
Cahyono Wahyu Setiawan <cahyonowahyus123@gmail.com>
2024-02-21 00:00:37 +0700

    Feature: add new descrpition of data feature project

--- client/src/constant/dataFeatureProject.ts
+++ client/src/constant/dataFeatureProject.ts
@@ -1,8 +1,14 @@
+import { StaticImageData } from "next/image";
+
+import AsianAgriImage from "../../assets/images/asian-agri.png";
+import ElangsungImage from "../../assets/images/elangsung.png";
+import KOINSImage from "../../assets/images/k-oins.png";
+
 export interface FeatureProjectProps {
   name: string;
   typeWork: string;
   typeProject: string;
-  urlImg: string;
+  urlImg: string | StaticImageData;
   urlProject: string;
   description: string;
 }
@@ -12,24 +18,26 @@ export const DataFeatureProject: FeatureProjectProps[] = [
     name: "Asian Agri App",
     typeProject: "Mobile App",
     typeWork: "Design Mobile App",
-    urlImg: "",
-    urlProject: "",
-    description: "",
+    urlImg: AsianAgriImage,
+    urlProject: "https://www.asianagri.com/en/",
+    description: "Has responsible for create new app for the ",
   },
   {
     name: "K-OINS",
     typeProject: "Mobile App",
     typeWork: "Mobile App & Dashboard",
-    urlImg: "",
-    urlProject: "",
+    urlImg: KOINSImage,
+    urlProject:
+      "https://play.google.com/store/apps/details?id=id.co.koneksindo.kadenceandroidpanel&hl=in",
     description: "",
   },
   {
     name: "E-Langsung",
     typeProject: "Mobile App",
     typeWork: "Mobile App & Dashboard",
-    urlImg: "",
-    urlProject: "",
+    urlImg: ElangsungImage,
+    urlProject:
+      "https://play.google.com/store/apps/details?id=com.elangsung.club&hl=en&gl=US",
     description: "",
   },
 ];
